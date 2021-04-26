101040337cefec0bb8268b6e477229ed7a6d8d7b
Tristan Adams <tristan@devbot.example.net>
2021-04-26 10:54:35 -0400

    Interupting IRQCOW?

--- src/proto-kernel.asm
+++ src/proto-kernel.asm
@@ -1,24 +1,56 @@
-;prototype kernel
+; prototype kernel (bleeding edge)
+[ORG 0x7c00]      ; add to offsets
+   jmp start
+ 
+   %include "src/print.inc"
+ 
+start:   xor ax, ax   ; make it zero
+   mov ds, ax   ; DS=0
+   mov ss, ax   ; stack starts at 0
+   mov sp, 0x9c00   ; 200h past code start
+ 
+   mov ax, 0xb800   ; text video memory
+   mov es, ax
+ 
+   cli      ;no interruptions
+   mov bx, 0x09   ;hardware interrupt #
+   shl bx, 2   ;multiply by 4
+   xor ax, ax
+   mov gs, ax   ;start of memory
+   mov [gs:bx], word keyhandler
+   mov [gs:bx+2], ds ; segment
+   sti
+ 
+   jmp $      ; loop forever
+ 
+keyhandler:
+   in al, 0x60   ; get key data
+   mov bl, al   ; save it
+   mov byte [port60], al
+ 
+   in al, 0x61   ; keybrd control
+   mov ah, al
+   or al, 0x80   ; disable bit 7
+   out 0x61, al   ; send it back
+   xchg ah, al   ; get original
+   out 0x61, al   ; send that back
+ 
+   mov al, 0x20   ; End of Interrupt
+   out 0x20, al   ;
+ 
+   and bl, 0x80   ; key released
+   jnz done   ; don't repeat
+ 
+   mov ax, [port60]
+   mov  word [reg16], ax
+   call printreg16
+ 
+done:
+   iret
+ 
+port60   dw 0
+ 
 
-    mov ax, 0x07c0
-    mov ds, ax
-    
-    mov si, msg
-    cld
-    
-ch_loop:lodsb
-    or al, al ; zero=end or str
-    jz hang ; get out
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
-    jmp ch_loop
-    
-hang:
-    jmp hang
-    
-    
-msg  db 'GUIDANCE ONLINE!', 13, 10, 0
-    times 510-($-$$) db 0
-    db 0x55
-    db 0xAA
+   times 510-($-$$) db 0  ; fill sector w/ 0's
+   dw 0xAA55        ; req'd by some BIOSes
+;==========================================
